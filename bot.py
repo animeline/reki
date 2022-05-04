afc5cd03cb74a5864169020aff6f2ef1ce9e76bf
izakdvlpr <izakdvlpr@gmail.com>
2022-05-04 20:39:53 -0300

    feat: create cogs

---
+++ bot.py
@@ -0,0 +1,64 @@
+import os
+import discord
+from discord.ext import commands 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Reki(commands.AutoShardedBot):
+  def __init__(self):
+    intents = discord.Intents(
+      messages=True,
+    )
+  
+    super().__init__(
+      command_prefix=custom_prefix,
+      intents=intents
+    )
+  
+  async def setup_cogs(self):
+    cogs = [c[:-3] for c in os.listdir('cogs') if c.endswith('.py')]
+    total_cogs = len(cogs)
+    
+    for i, cog in enumerate(cogs, 1):
+      try:
+        self.load_extension('cogs.' + cog)
+      except Exception as e:
+        print(f'Falha ao carregar a Cog \'{cog}\':')
+      else:
+        print(f'Cog {cog} carregada com sucesso. ({i}/{total_cogs})')
+        
+        
+  async def on_ready(self):
+    await self.setup_cogs()
+    
+    print(f'Ready: {self.user} (ID: {self.user.id})')
+    
+  async def on_message(self, message):
+    ctx = await self.get_context(message)
+    
+    if ctx.command is None:
+      return
+    
+    await self.invoke(ctx)
+  
+  def run(self):
+    super().run(os.environ['DISCORD_TOKEN'])
+    
+def custom_prefix(bot: Reki, msg: discord.Message):
+  user_id = bot.user.id
+  base = [f'<@!{user_id}> ', f'<@{user_id}> ']
+  
+  if msg.guild is None:
+    base.append('!')
+    base.append('?')
+  else:
+    base.extend(bot.prefixes.get(msg.guild.id, ['?', '!']))
+    
+  return base
+
+if __name__ == '__main__':
+  bot = Reki()
+  bot.run()
+  
---
+++ cogs/info.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+from bot import Reki 
+
+class Information(commands.Cog):
+  def __init__(self, reki):
+    self.reki = reki
+    
+  @commands.command(name='ping', aliases=['latency'])
+  async def _ping(self, ctx):
+    await ctx.send('Pong!')
+        
+
+def setup(reki: Reki):
+  reki.add_cog(Information(reki))
